f8ae153531b67f2006e060a4e0cab6d1beee4ed6
Agam1410 <48292808+Agam1410@users.noreply.github.com>
2023-06-27 18:22:26 -0400

    Update AC_AttitudeControl_Multi.cpp

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
@@ -390,6 +390,32 @@ void AC_AttitudeControl_Multi::rate_controller_run()
     control_monitor_update();
 }
 
+// if the mode is MFC use the new function - update_all_mfc
+void AC_AttitudeControl_Multi::rate_controller_run_mfc()
+{
+
+    update_throttle_rpy_mix();
+
+    _ang_vel_body += _sysid_ang_vel_body;
+
+    Vector3f gyro_latest = _ahrs.get_gyro_latest();
+    
+    _motors.set_roll(get_rate_roll_pid().update_all_mfc(_ang_vel_body.x, gyro_latest.x, _motors.limit.roll) + _actuator_sysid.x);
+    _motors.set_roll_ff(get_rate_roll_pid().get_ff());
+
+    _motors.set_pitch(get_rate_pitch_pid().update_all_mfc(_ang_vel_body.y, gyro_latest.y, _motors.limit.pitch) + _actuator_sysid.y);
+    _motors.set_pitch_ff(get_rate_pitch_pid().get_ff());
+
+    _motors.set_yaw(get_rate_yaw_pid().update_all(_ang_vel_body.z, gyro_latest.z, _motors.limit.yaw) + _actuator_sysid.z);
+    _motors.set_yaw_ff(get_rate_yaw_pid().get_ff()*_feedforward_scalar);
+
+    _sysid_ang_vel_body.zero();
+    _actuator_sysid.zero();
+
+    control_monitor_update();
+
+};
+
 // sanity check parameters.  should be called once before takeoff
 void AC_AttitudeControl_Multi::parameter_sanity_check()
 {
